df54f46f9200562031750265ba4236ae0eddb4ec
xSlapppz <braden.whaley7@gmail.com>
2017-02-01 03:02:54 -0500

    Code optimization

--- Indicator++/Indicator++.cpp
+++ Indicator++/Indicator++.cpp
@@ -11,13 +11,9 @@ IMenu* MainMenu;
 IMenu* Options;
 
 IMenuOption* Q;
-IMenuOption* QType;
 IMenuOption* W;
-IMenuOption* WType;
 IMenuOption* E;
-IMenuOption* EType;
 IMenuOption* R;
-IMenuOption* RType;
 IMenuOption* IG;
 IMenuOption* ColorR;
 IMenuOption* ColorG;
@@ -34,13 +30,9 @@ void Menu()
 	Options = MainMenu->AddMenu("Options");
 	AA = Options->AddInteger("Add Auto Attacks",0,15,0);
 	Q = Options->CheckBox("Q",true);
-	QType = Options->AddInteger("Q Damage Type", 1, 3, 1);
 	W = Options->CheckBox("W", true);
-	WType = Options->AddInteger("W Damage Type", 1, 3, 1);
 	E = Options->CheckBox("E", true);
-	EType = Options->AddInteger("E Damage Type", 1, 3, 1);
 	R = Options->CheckBox("R", true);
-	RType = Options->AddInteger("R Damage Type", 1, 3, 1);
 	IG = Options->CheckBox("Ignite", true);
 	ColorR = Options->AddInteger("Red", 0, 255, 67);
 	ColorG = Options->AddInteger("Green", 0, 255, 80);
@@ -61,61 +53,13 @@ double CalculateComboDamage(IUnit* enemy)
 	double dmg = 0;
 	
 	if (Q->Enabled() && Qspell->IsReady())
-	{
-		switch (QType->GetInteger())
-		{
-		case 1:
-			dmg += GDamage->CalcPhysicalDamage(GEntityList->Player(), enemy, GDamage->GetSpellDamage(GEntityList->Player(), enemy, kSlotQ));
-			break;
-		case 2:
-			dmg += GDamage->CalcMagicDamage(GEntityList->Player(), enemy, GDamage->GetSpellDamage(GEntityList->Player(), enemy, kSlotQ));
-			break;
-		case 3:
-			dmg += GDamage->GetSpellDamage(GEntityList->Player(), enemy, kSlotQ);
-		}
-	}
+		dmg += GDamage->GetSpellDamage(GEntityList->Player(), enemy, kSlotQ);
 	if (W->Enabled() && Wspell->IsReady())
-	{
-		switch (WType->GetInteger())
-		{
-		case 1:
-			dmg += GDamage->CalcPhysicalDamage(GEntityList->Player(), enemy, GDamage->GetSpellDamage(GEntityList->Player(), enemy, kSlotW));
-			break;
-		case 2:
-			dmg += GDamage->CalcMagicDamage(GEntityList->Player(), enemy, GDamage->GetSpellDamage(GEntityList->Player(), enemy, kSlotW));
-			break;
-		case 3:
-			dmg += GDamage->GetSpellDamage(GEntityList->Player(), enemy, kSlotW);
-		}
-	}
+		dmg += GDamage->GetSpellDamage(GEntityList->Player(), enemy, kSlotW);
 	if (E->Enabled() && Espell->IsReady())
-	{
-		switch (EType->GetInteger())
-		{
-		case 1:
-			dmg += GDamage->CalcPhysicalDamage(GEntityList->Player(), enemy, GDamage->GetSpellDamage(GEntityList->Player(), enemy, kSlotE));
-			break;
-		case 2:
-			dmg += GDamage->CalcMagicDamage(GEntityList->Player(), enemy, GDamage->GetSpellDamage(GEntityList->Player(), enemy, kSlotE));
-			break;
-		case 3:
-			dmg += GDamage->GetSpellDamage(GEntityList->Player(), enemy, kSlotE);
-		}
-	}
+		dmg += GDamage->GetSpellDamage(GEntityList->Player(), enemy, kSlotE);
 	if (R->Enabled() && Rspell->IsReady())
-	{
-		switch (RType->GetInteger())
-		{
-		case 1:
-			dmg += GDamage->CalcPhysicalDamage(GEntityList->Player(), enemy, GDamage->GetSpellDamage(GEntityList->Player(), enemy, kSlotR));
-			break;
-		case 2:
-			dmg += GDamage->CalcMagicDamage(GEntityList->Player(), enemy, GDamage->GetSpellDamage(GEntityList->Player(), enemy, kSlotR));
-			break;
-		case 3:
-			dmg += GDamage->GetSpellDamage(GEntityList->Player(), enemy, kSlotR);
-		}
-	}
+		dmg += GDamage->GetSpellDamage(GEntityList->Player(), enemy, kSlotR);
 	if (Ignite->GetSpellSlot() != kSlotUnknown && Ignite->IsReady())
 		dmg += GDamage->GetSummonerSpellDamage(GEntityList->Player(), enemy, kSummonerSpellIgnite);
 	if (AA->GetInteger() > 0)
@@ -143,9 +87,6 @@ PLUGIN_API void OnLoad(IPluginSDK* PluginSDK)
 	Menu();
 	Spells();
 	GGame->PrintChat("Indicator++ Loaded!");
-	GGame->PrintChat("1 - Physical Damage");
-	GGame->PrintChat("2 - Spell Damage");
-	GGame->PrintChat("3 - True Damage");
 
 	GEventManager->AddEventHandler(kEventOnRender, OnRender);
 
